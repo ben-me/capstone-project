b9cd80d64202b0c00733711c3fcebda71b56e21d
Ben Me <benedikt.mehler@mailbox.org>
2022-09-20 10:55:51 +0200

    create button on homepage and link to page rooms

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+import { React, useState } from 'react';
+import styled from 'styled-components';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container>
+      <Link href="/rooms">
+        <Button>Show all rooms</Button>
+      </Link>
+    </Container>
+  );
 }
+
+const Button = styled.button`
+  padding: 3em;
+  font-size: xx-large;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
