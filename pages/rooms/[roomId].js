15f71200ce465c55ba0f24f0e0801d00aa5e619c
Ben Me <benedikt.mehler@mailbox.org>
2022-09-21 17:49:41 +0200

    create dynamic route for single room

--- pages/rooms/[roomId].js
+++ pages/rooms/[roomId].js
@@ -1,11 +1,52 @@
 import { useRouter } from 'next/router';
-import { getRoomById } from '../../services/roomService';
+import styled from 'styled-components';
+import BackButton from '../../components/BackButton';
+import DeskItem from '../../components/DeskItem';
+import Header from '../../components/Header';
+import { getAllRooms, getRoomById } from '../../services/roomService';
 
-export default function RoomPage() {
+export async function getStaticPaths() {
+  const allRooms = await getAllRooms();
+  const paths = allRooms.map((rooms) => {
+    return { params: { roomId: rooms.id } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const roomId = context.params.roomId;
+  const roomDetails = await getRoomById(roomId);
+
+  return {
+    props: { roomDetails },
+  };
+}
+
+export default function RoomPage({ roomDetails }) {
   const router = useRouter();
   const { roomId } = router.query;
-  const roomData = getRoomById(roomId);
-  console.log(roomData);
 
-  return <p>This is Room {roomId}</p>;
+  return (
+    <>
+      <BackButton page={'/rooms'} />
+      <Header title={roomDetails.name} />
+      <DeskList>
+        <DeskItem />
+      </DeskList>
+    </>
+  );
 }
+
+const DeskList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  gap: 51px;
+  margin: 2.7em auto;
+  width: 375px;
+  left: 26px;
+`;
