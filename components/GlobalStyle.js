e07ad6d026591c4f41b812a9078bac8703630c33
Ben Me <benedikt.mehler@mailbox.org>
2022-09-21 15:31:42 +0200

    recover data after making merge revert

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -4,11 +4,9 @@ export const GlobalStyle = createGlobalStyle`
 *,*::after, *::before {
     box-sizing:border-box;
 }
-
 body {
     margin: 0;
     padding: 0;
+    font-family: 'Inter', sans-serif;
 }
-
-
 `;
