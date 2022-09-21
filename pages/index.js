f055a7cd4e1a5dd5737a7f713f0b4683c976094d
Ben Me <benedikt.mehler@mailbox.org>
2022-09-21 13:41:13 +0200

    replace svg with image, update buttons to be a-tags, add li around link and add styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
 import Link from 'next/link';
-import { React, useState } from 'react';
 import styled from 'styled-components';
 
 export default function Home() {
   return (
     <Container>
-      <Link href="/rooms">
-        <Button>Show all rooms</Button>
+      <Link href="/rooms" passHref>
+        <Anchor>Show all rooms</Anchor>
       </Link>
     </Container>
   );
 }
 
-const Button = styled.button`
-  padding: 3em;
+const Anchor = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 100px;
+  padding: 1em;
+  max-width: 310px;
   font-size: xx-large;
+  border: 1px rgba(101, 181, 255, 1) solid;
+  border-radius: 15px;
+  box-shadow: 0px 4px 4px rgba(101, 181, 255, 0.4);
+  background-color: white;
+  transition: 0.4s;
+  text-decoration: none;
+  color: black;
+  text-align: center;
+
+  &:hover {
+    background-color: rgba(101, 181, 255, 1);
+  }
 `;
 
 const Container = styled.div`
--- pages/rooms.js
+++ pages/rooms.js
@@ -2,9 +2,11 @@ import RoomItem from '../components/RoomItem';
 import { getAllRooms } from '../services/roomService';
 import styled from 'styled-components';
 import Link from 'next/link';
+import Image from 'next/image';
+import backArrow from '../public/backarrow.png';
 
-export function getStaticProps() {
-  const rooms = getAllRooms();
+export async function getStaticProps() {
+  const rooms = await getAllRooms();
   return {
     props: {
       rooms,
@@ -15,22 +17,14 @@ export function getStaticProps() {
 export default function Rooms({ rooms }) {
   return (
     <>
-      <Link href="/">
-        <StyleSVG
-          width="17"
-          height="34"
-          viewBox="0 0 17 34"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14.27 33.3333C13.9214 33.3345 13.577 33.2576 13.262 33.1082C12.9471 32.9588 12.6696 32.7407 12.45 32.47L1.18001 18.47C0.836815 18.0525 0.6492 17.5288 0.6492 16.9883C0.6492 16.4479 0.836815 15.9242 1.18001 15.5067L12.8467 1.50668C13.2427 1.03017 13.8119 0.730517 14.4288 0.673631C15.0458 0.616745 15.6602 0.807289 16.1367 1.20335C16.6132 1.5994 16.9128 2.16853 16.9697 2.78552C17.0266 3.40252 16.8361 4.01684 16.44 4.49335L6.01 17L16.09 29.5067C16.3753 29.8492 16.5566 30.2662 16.6123 30.7085C16.668 31.1508 16.5959 31.5998 16.4044 32.0024C16.2129 32.4049 15.9102 32.7442 15.5319 32.9801C15.1537 33.216 14.7158 33.3386 14.27 33.3333Z"
-            fill="#CCCCCC"
-          />
-        </StyleSVG>
+      <Link href="/" passHref>
+        <BackAnchor>
+          <Image alt="backbutton" src={backArrow} />
+        </BackAnchor>
       </Link>
-      <Header>All Rooms</Header>
+
       <RoomList>
+        <Header>All Rooms</Header>
         {rooms.map((room) => {
           return <RoomItem key={room.id} roomData={room} />;
         })}
@@ -42,23 +36,24 @@ export default function Rooms({ rooms }) {
 const RoomList = styled.ul`
   display: flex;
   flex-direction: column;
-  align-items: center;
   padding: 0;
   gap: 51px;
-  position: absolute;
+  /* position: absolute; */
+  margin: 2.7em auto;
   width: 375px;
-  height: 819.01px;
   left: 26px;
 `;
 
 const Header = styled.h1`
   text-align: center;
-  margin-top: 60px;
-  margin-bottom: 0px;
+  margin-bottom: 0;
+  margin-top: 0;
 `;
 
-const StyleSVG = styled.svg`
+const BackAnchor = styled.a`
+  display: inline-block;
   position: absolute;
-  left: 20px;
-  top: 30px;
+  left: 0px;
+  z-index: 1;
+  top: 20px;
 `;
