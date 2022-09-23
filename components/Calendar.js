6b871dadcc2b859d19f0394ce6cfb349cb61898a
Ben Me <benedikt.mehler@mailbox.org>
2022-09-23 16:00:55 +0200

    fix font bug, change calendar display

--- components/Calendar.js
+++ components/Calendar.js
@@ -19,6 +19,7 @@ export default function Calendar({ stateDate, onChangeDate }) {
       calendarStartDay={1}
       withPortal
       minDate={new Date()}
+      popperPlacement="bottom-end"
     />
   );
 }
