23db8c8ef0cd037839bd56aa560015142c74e280
Ben Me <benedikt.mehler@mailbox.org>
2022-09-30 16:23:24 +0200

    update infobox

--- components/DeskInfoBox.js
+++ components/DeskInfoBox.js
@@ -39,15 +39,11 @@ export default function DeskInfoBox({
 }
 
 const Infobox = styled.ul`
-  width: 21.875rem;
-  position: fixed;
+  width: 100%;
   padding-left: 1.25rem;
-  transform: translate(-50%);
   border: 1px solid black;
   border-radius: 7px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  left: 50%;
-  bottom: 3.75rem;
 `;
 
 const ReservationHeader = styled.h4`
@@ -61,6 +57,6 @@ const CloseButton = styled.button`
   border: none;
   width: 1.25rem;
   height: 1.25rem;
-  top: 0;
-  right: 0.313rem;
+  top: 1rem;
+  right: 0.5rem;
 `;
