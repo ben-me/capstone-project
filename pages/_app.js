46fce3f10f7fd51da4c1b17f7b7cb98ed35d3ee7
Ben Me <benedikt.mehler@mailbox.org>
2022-10-07 17:39:43 +0200

    fix state in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,14 @@
 import { useState } from 'react';
 import { GlobalStyle } from '../components/GlobalStyle';
-import { getAllRooms, getReservationByUser } from '../services/roomService';
+import { getAllRooms } from '../services/roomService';
 
 function MyApp({ Component, pageProps }) {
-  const [userReservations, setUserReservations] = useState(() =>
-    getReservationByUser()
-  );
   const [allRooms, setAllRooms] = useState(() => getAllRooms());
 
   return (
     <>
       <GlobalStyle />
-      <Component
-        {...pageProps}
-        allRooms={allRooms}
-        onSetAllRooms={setAllRooms}
-        userReservations={userReservations}
-        onSetUserReservations={setUserReservations}
-      />
+      <Component {...pageProps} allRooms={allRooms} setAllRooms={setAllRooms} />
     </>
   );
 }
