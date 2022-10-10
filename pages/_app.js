314c4e25936ca6c07ec4f5ec078b5379fd9b2b9c
Ben Me <benedikt.mehler@mailbox.org>
2022-10-10 12:11:47 +0200

    remove global state

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
-import { useState } from 'react';
 import { GlobalStyle } from '../components/GlobalStyle';
-import { getAllRooms } from '../services/roomService';
 
 function MyApp({ Component, pageProps }) {
-  const [allRooms, setAllRooms] = useState(() => getAllRooms());
-
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} allRooms={allRooms} setAllRooms={setAllRooms} />
+      <Component {...pageProps} />
     </>
   );
 }
