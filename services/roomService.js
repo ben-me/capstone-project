336c0f6a726dd261606bc0806c39db4ca01b642d
Ben Me <benedikt.mehler@mailbox.org>
2022-10-10 12:12:12 +0200

    rewrite roomServices for database

--- services/roomService.js
+++ services/roomService.js
@@ -1,224 +1,36 @@
-const rooms = [
-  {
-    id: '1',
-    name: 'Room 1',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [
-          {
-            id: '1',
-            starttime: '08:00',
-            endtime: '10:00',
-            date: '2022-09-29',
-            user: 'user1',
-            isPrivate: false,
-          },
-          {
-            id: '2',
-            starttime: '10:00',
-            endtime: '12:00',
-            date: '2022-09-29',
-            user: 'user1',
-            isPrivate: false,
-          },
-          {
-            id: '3',
-            starttime: '12:00',
-            endtime: '14:00',
-            date: '2022-09-29',
-            user: 'user1',
-            isPrivate: false,
-          },
-          {
-            id: '4',
-            starttime: '14:00',
-            endtime: '16:00',
-            date: '2022-09-29',
-            user: 'user1',
-            isPrivate: false,
-          },
-        ],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [
-          {
-            id: '1',
-            starttime: '15:00',
-            endtime: '17:00',
-            date: '2022-09-30',
-            user: 'user2',
-            isPrivate: false,
-          },
-        ],
-      },
-      {
-        id: '3',
-        name: 'Desk 3',
-        reservations: [],
-      },
-      {
-        id: '4',
-        name: 'Desk 4',
-        reservations: [],
-      },
-      {
-        id: '5',
-        name: 'Desk 5',
-        reservations: [],
-      },
-      {
-        id: '6',
-        name: 'Desk 6',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '2',
-    name: 'Room 2',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '3',
-    name: 'Room 3',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '4',
-    name: 'Room 4',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-      {
-        id: '3',
-        name: 'Desk 3',
-        reservations: [],
-      },
-      {
-        id: '4',
-        name: 'Desk 4',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '5',
-    name: 'Room 5',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [
-          {
-            id: '6',
-            starttime: '14:00',
-            endtime: '16:00',
-            date: '2022-09-28',
-            user: 'user1',
-            isPrivate: false,
-          },
-        ],
-      },
-      {
-        id: '3',
-        name: 'Desk 3',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '6',
-    name: 'Room 6',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '7',
-    name: 'Room 7',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-    ],
-  },
-  {
-    id: '8',
-    name: 'Room 8',
-    desks: [
-      {
-        id: '1',
-        name: 'Desk 1',
-        reservations: [],
-      },
-      {
-        id: '2',
-        name: 'Desk 2',
-        reservations: [],
-      },
-    ],
-  },
-];
+import dbConnect from '../dbConnect';
+import Room from '../models/Room';
 
-export function getAllRooms() {
-  return rooms;
+export default async function getAllRooms() {
+  await dbConnect();
+  const rooms = await Room.find();
+  const roomArray = rooms.map(({ id, name, desks }) => {
+    return {
+      id,
+      name,
+      desks: desks.map(({ name, reservations }) => {
+        return {
+          name,
+          reservations,
+        };
+      }),
+    };
+  });
+  return roomArray;
 }
 
-export function getRoomById(id) {
-  return rooms.find((room) => room.id === id);
+export async function getRoomById(roomId) {
+  await dbConnect();
+  const room = await Room.findById(roomId);
+  const { id, name, desks } = room;
+  return {
+    id,
+    name,
+    desks: desks.map(({ name, reservations }) => {
+      return {
+        name,
+        reservations,
+      };
+    }),
+  };
 }
