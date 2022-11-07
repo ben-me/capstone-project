3859c579c57bf7a28fb9cd1fbd148afdc1508f41
Ben Me <benedikt.mehler@mailbox.org>
2022-11-07 15:29:25 +0100

    update dbconnect file

--- dbConnect.js
+++ dbConnect.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 
-export default async function dbConnect() {
+export default function dbConnect() {
   const { MONGODB_URL } = process.env;
 
   try {
-    await mongoose.connect(MONGODB_URL);
+    if (mongoose.connections[0].readyState) {
+      console.log('Already connected');
+      return;
+    }
+    mongoose.connect(MONGODB_URL);
     console.log('Connected!');
   } catch (error) {
     console.log('Connection failed!');
