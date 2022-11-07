66c64524343e01738ca51dae1129507731509d64
Ben Me <benedikt.mehler@mailbox.org>
2022-11-07 15:29:36 +0100

    update user model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ import mongoose, { Schema } from 'mongoose';
 const userSchema = new Schema({
   name: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
 });
 
-const User = mongoose.models.Login || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
