e8d5b5c9e854711906d00b1b4622c224573699cd
Ben Me <benedikt.mehler@mailbox.org>
2022-11-28 23:19:03 +0100

    prototype for bcrypt with mongoose

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import next from 'next';
 
 const userSchema = new Schema({
   name: { type: String, required: true, unique: true },
@@ -9,3 +11,18 @@ const userSchema = new Schema({
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
+
+userSchema.pre('save', async function () {
+  await bcrypt.genSalt(10, function (error, salt) {
+    if (error) {
+      console.log(error);
+    } else {
+      bcrypt.hash(this.password, salt, function (err, hash) {
+        if (err) {
+          console.log(err);
+        }
+        saveUser(hash);
+      });
+    }
+  });
+});
