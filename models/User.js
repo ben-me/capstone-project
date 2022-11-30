e571aa9c9f314daf5dbec8664d51898318c0bdbe
Ben Me <benedikt.mehler@mailbox.org>
2022-11-30 20:56:46 +0100

    create api to register user and remove part from mongoose model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,4 @@
 import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcryptjs';
-import next from 'next';
 
 const userSchema = new Schema({
   name: { type: String, required: true, unique: true },
@@ -11,25 +9,3 @@ const userSchema = new Schema({
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
-
-userSchema.pre('save', async function () {
-  const user = this;
-
-  if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, function (error, salt) {
-      if (error) {
-        return next(error);
-      } else {
-        bcrypt.hash(user.password, salt, function (hashError, hash) {
-          if (hashError) {
-            return next(hashError);
-          }
-          user.password = hash;
-          next();
-        });
-      }
-    });
-  } else {
-    return next();
-  }
-});
